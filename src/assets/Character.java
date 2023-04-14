d366483325729fe94f7c1fd3ffd406687898f7ad
Carlos Daniel <ms_danniel@outlook.com>
2023-04-14 18:18:21 -0300

    criada thread para movimento

--- src/assets/Character.java
+++ src/assets/Character.java
@@ -48,23 +48,32 @@ public class Character extends JLabel{
 		String first = "lufy-1-1";
 		String second = "lufy-1-2";
 		String stop = "lufy-1";
+
+        // Criação da thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+            	for(int i = 0; i < 100; i++) {
+        			if(i%2 != 0)
+        				skin = get.getIcon("character/luffy/" + first + ".png");
+        			else
+        				skin = get.getIcon("character/luffy/" + second + ".png");
+        			
+        			setIcon(skin);
+        			try {
+        				Thread.sleep(300);
+        			} catch (InterruptedException e) {
+        				// TODO Auto-generated catch block
+        				e.printStackTrace();
+        			}
+        		}
+        		
+        		skin = get.getIcon("character/luffy/" + stop + ".png");
+        		setIcon(skin);
+            }
+        }).start();
 		
-		for(int i = 0; i < 100; i++) {
-			if(i%2 != 0)
-				this.skin = get.getIcon("character/luffy/" + first + ".png");
-			else
-				this.skin = get.getIcon("character/luffy/" + second + ".png");
-			
-			setIcon(skin);
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 		
-		this.skin = get.getIcon("character/luffy/" + stop + ".png");
 	}
 	
 	public void Resize(int width, int height) {
