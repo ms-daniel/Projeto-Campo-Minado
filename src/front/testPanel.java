458c5714e1a816c7a1636248c7dc7374e182ebe5
Carlos Daniel <ms_danniel@outlook.com>
2023-04-15 02:39:55 -0300

    movimentacao apenas com setas

--- src/front/testPanel.java
+++ src/front/testPanel.java
@@ -34,6 +34,7 @@ public class testPanel extends JPanel implements KeyListener {
 	private ImagesChange get = new ImagesChange();
 	
 	private Character character;
+	private JLabel labelCharacter;
 	private JButton leftButton;
 	private JButton rightButton;
 	private JButton playButton;
@@ -50,59 +51,31 @@ public class testPanel extends JPanel implements KeyListener {
 		setBounds(new Rectangle(0, 0, 500, 500));
 		setLayout(null);
 		
-		character = new Character("Luffy", "character/luffy/" + "lufy-1" + ".png");
+		labelCharacter = new JLabel();
+		
+		character = new Character("Luffy", "character/luffy/" + "lufy-1" + ".png", labelCharacter);
 		character.Locale(213, 128);
+		character.start(); 
+
+		JLabel arrowkeyslabel = new JLabel("");
+		arrowkeyslabel.setToolTipText("keys");
+		arrowkeyslabel.setIcon(get.getIcon("icons/", "arrowskeys", ".png"));
+		arrowkeyslabel.setBounds(0, 409, 205, 137);
 		
-		leftButton = new JButton("");
-		leftButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		leftButton.setContentAreaFilled(false);
-		leftButton.setBorderPainted(false);
-		leftButton.setFocusPainted(false);
-		leftButton.setBorder(null);
-		leftButton.setBounds(new Rectangle(253, 391, 35, 35));
-		leftButton.setMargin(new Insets(0, 0, 0, 0));
-		leftButton.setToolTipText("left");
-		leftButton.setIcon(get.getIcon("icons/", "next-left", ".png"));
-		
-		
-		rightButton = new JButton("");
-		rightButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		rightButton.setContentAreaFilled(false);
-		rightButton.setBorderPainted(false);
-		rightButton.setFocusPainted(false);
-		rightButton.setToolTipText("right");
-		rightButton.setMargin(new Insets(0, 0, 0, 0));
-		rightButton.setBounds(new Rectangle(324, 391, 35, 35));
-		rightButton.setBorder(null);
-		rightButton.setIcon(get.getIcon("icons/", "next-right", ".png"));
-		
-		playButton = new JButton("");
-		playButton.setToolTipText("play");
-		playButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		playButton.setMargin(new Insets(0, 0, 0, 0));
-		playButton.setFocusPainted(false);
-		playButton.setContentAreaFilled(false);
-		playButton.setBounds(new Rectangle(324, 391, 35, 35));
-		playButton.setBorderPainted(false);
-		playButton.setBorder(null);
-		playButton.setBounds(287, 391, 35, 35);
-		playButton.setIcon(get.getIcon("icons/", "play", ".png"));
-		
-		add(rightButton);
-		add(character);
-		add(leftButton);
-		add(playButton);
+		add(labelCharacter);
+
 		
 		//permite que seja identificada teclas pressionadas no panel
 		addKeyListener(this);
 		setFocusable(true); //so funciona com essa opcao ativada
+		
+		add(arrowkeyslabel);
 
 		//
-		
-		effects();
+		//effects();
 	}
 	
-	private void effects() {
+	/*private void effects() {
 		leftButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -111,7 +84,7 @@ public class testPanel extends JPanel implements KeyListener {
 				else
 					atual = 4;
 				
-				character.setIcon(get.getIcon("character/luffy/", name + atual, ".png"));
+				labelCharacter.setIcon(get.getIcon("character/luffy/", name + atual, ".png"));
 				requestFocus(); //da o foco ao painel/janela dps q pressionar o botao
 			}
 		});
@@ -132,11 +105,11 @@ public class testPanel extends JPanel implements KeyListener {
 		playButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				character.Move();
+				//character.Move();
 			}
 		});
 	}
-	
+	*/
 	@Override
     public void keyTyped(KeyEvent e) {
         // Implemente o método keyTyped se necessário
@@ -146,22 +119,17 @@ public class testPanel extends JPanel implements KeyListener {
     public void keyPressed(KeyEvent e) {
         // Verifique se a tecla pressionada é a tecla desejada
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            // Abra a caixa de diálogo quando a tecla F1 for pressionada
-        	if(atual < 4)
-				atual++;
-			else
-				atual = 1;
-		
-        	character.setIcon(get.getIcon("character/luffy/", name + atual, ".png"));
+        	character.MoveTo('R', 2);
+        	
+        }else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+        	character.MoveTo('L', 4);
+
+        }else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+        	character.MoveTo('D', 1);
+      
         }
-        
-        else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-        	if(atual > 1)
-				atual--;
-			else
-				atual = 4;
-			
-        	character.setIcon(get.getIcon("character/luffy/", name + atual, ".png"));
+        else if(e.getKeyCode() == KeyEvent.VK_UP) {
+        	character.MoveTo('T', 3);
         }
     }
 
