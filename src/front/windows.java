78df3253d11315fbb6eff04d138d9352bb89bc1b
Carlos Daniel <ms_danniel@outlook.com>
2023-04-21 11:59:36 -0300

    tamanho da janela

--- src/front/windows.java
+++ src/front/windows.java
@@ -32,7 +32,7 @@ public class windows extends JFrame {
 	public windows() {
 		setTitle("Campo Minado");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 600);
+		setBounds(0, 0, 646, 669);
 		contentPane = new playGame();
 		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
