849f262e6e9d2c99f30244261d20b8f9672356a9
Carlos Daniel <ms_danniel@outlook.com>
2023-04-24 16:41:04 -0300

    novos botoes

--- src/front/Menu.java
+++ src/front/Menu.java
@@ -13,6 +13,9 @@ import java.awt.Cursor;
 public class Menu extends JPanel {
 
 	private ImagesChange get = new ImagesChange();
+	private JLabel Conectar;
+	private JLabel CriarPartida;
+	private JLabel MenuBackground;
 	
 	/**
 	 * Create the panel.
@@ -20,8 +23,28 @@ public class Menu extends JPanel {
 	public Menu() {
 		setLayout(null);
 		
-		JLabel Conectar = new JLabel("New label");
+		Conectar = new JLabel("");
 		Conectar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		Conectar.setIcon(get.getIcon("icons/Conectar.png"));
+		Conectar.setBorder(null);
+		Conectar.setBounds(67, 205, 300, 45);
+		add(Conectar);
+		
+		CriarPartida = new JLabel("");
+		CriarPartida.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		CriarPartida.setIcon(get.getIcon("icons/Criar Partida.png"));
+		CriarPartida.setBorder(null);
+		CriarPartida.setBounds(67, 270, 465, 45);
+		add(CriarPartida);
+		
+		MenuBackground = new JLabel("");
+		MenuBackground.setIcon(new ImageIcon(Menu.class.getResource("/icons/menureflexc.jpg")));
+		MenuBackground.setBounds(0, 0, 630, 630);
+		//add(MenuBackground);
+
+	}
+	
+	private void EffectsOn() {
 		Conectar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
@@ -32,15 +55,16 @@ public class Menu extends JPanel {
 				Conectar.setIcon(get.getIcon("icons/Conectar.png"));
 			}
 		});
-		Conectar.setIcon(get.getIcon("icons/Conectar.png"));
-		Conectar.setBorder(null);
-		Conectar.setBounds(67, 205, 300, 45);
-		add(Conectar);
 		
-		JLabel MenuBackground = new JLabel("");
-		MenuBackground.setIcon(get.getIcon("icons/menu.jpg"));
-		MenuBackground.setBounds(0, 0, 630, 630);
-		add(MenuBackground);
-
+		CriarPartida.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				CriarPartida.setIcon(get.getIcon("icons/Criar PartidaSelected.png"));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				CriarPartida.setIcon(get.getIcon("icons/Criar Partida.png"));
+			}
+		});
 	}
 }
