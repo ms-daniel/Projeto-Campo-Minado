ce166f052c4b36576303ad5eb834e6a06203d18d
Carlos Daniel <ms_danniel@outlook.com>
2023-04-27 13:52:12 -0300

    leitura de gif imagem a imagem

--- src/back/BombMenu.java
+++ src/back/BombMenu.java
@@ -1,26 +1,46 @@
 package back;
 
 import java.awt.Component;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class BombMenu extends Thread{
 	private ImagesChange get = new ImagesChange();
+	ArrayList<BufferedImage> images;
+	
 	private Timer timer;
+	
+	private Thread thread;
 	
 	private JPanel Menu;
 	private JLabel Bomb;
 	
+	private int cont = 15;
+	
 	public BombMenu(JPanel Menu, JLabel Bomb) {
-		timer = new Timer(1000, new ActionListener() {
+		try {
+			images = get.GetAllImagesGif("icons/bomb-gif.gif");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		
+		timer = new Timer(300, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Lógica do processamento do evento de teclado aqui
-                timer.stop();
+            	
+            	timer.stop();
             }
         });
 		
@@ -30,10 +50,21 @@ public class BombMenu extends Thread{
 	
 	@Override
 	public synchronized void run() {
-		int cont = 1;
-		while(timer.isRunning()) {
-			cont++;
+		while(cont < 30) {
+			ImageIcon icon = new ImageIcon(images.get(cont));
+			Bomb.setIcon(icon);
+        	cont++;
+        	
+        	try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
+		
+		cont = 15;
 		
 		EnableComponents();
 		this.interrupt();
@@ -43,17 +74,22 @@ public class BombMenu extends Thread{
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			this.Menu.removeAll();
 		}
 	}
 	
 	public synchronized void Explose() {
+		ImageIcon icon = new ImageIcon(images.get(cont));
+		Bomb.setIcon(icon);
 		Bomb.setVisible(true);
-		Bomb.setIcon(get.getIcon("icons/bomb-gif.gif"));
+		
 		
 		DisableComponents();
 		
 		timer.start(); //inicia timer
-		new Thread(this).start();
+		thread = new Thread(this);
+		thread.start();
 	}
 	
 	public boolean IsRunning() {
