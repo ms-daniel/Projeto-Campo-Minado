43f4df5aa128264ac980503b05ae10c57579e139
Carlos Daniel <ms_danniel@outlook.com>
2023-05-02 20:24:06 -0300

    adicionada função para movimentar todos os bonecos secundarios

--- src/back/MapMove.java
+++ src/back/MapMove.java
@@ -1,6 +1,9 @@
 package back;
 
+import java.util.ArrayList;
+
 import javax.swing.JLabel;
+import assets.Character;
 
 public class MapMove implements Runnable {
 	private JLabel map;
@@ -8,6 +11,7 @@ public class MapMove implements Runnable {
     private int x;
     private int y;
     private int increment;
+    private ArrayList<Character> characters = new ArrayList<>();;
     
 	public MapMove(JLabel map, JLabel mapT) {
         this.map = map;
@@ -30,6 +34,10 @@ public class MapMove implements Runnable {
 		}
 	}
 	
+	public void AddCharacterToMap(Character Character) {
+		this.characters.add(Character);
+	}
+	
 	@Override
 	public void run() {
 		//System.out.println("X: " + map.getX() + " Y: " + map.getY());
@@ -41,10 +49,14 @@ public class MapMove implements Runnable {
 				map.setLocation(map.getX() + increment, map.getY());
 				if(mapT != null)
 					mapT.setLocation(mapT.getX() + increment, mapT.getY());
+				
+				MoveAllCharacters(increment, 0);
 			}else {
 				map.setLocation(map.getX(), map.getY() + increment);
 				if(mapT != null)
 					mapT.setLocation(mapT.getX(), mapT.getY() + increment);	
+				
+				MoveAllCharacters(0, increment);
 			}
 
 			try {
@@ -56,10 +68,35 @@ public class MapMove implements Runnable {
 		}
 		
 		map.setLocation(xAux, yAux);
+		
+		//posiciona todos os players dentro do mapa nos seus lugares
+		if(y == 0)
+			setLocaleToAllCharacters(45*increment, 0);
+		else
+			setLocaleToAllCharacters(0, 45*increment);
+		
 		if(mapT != null)
 			mapT.setLocation(xAux, yAux);	
 		//System.out.println("depois X: " + map.getX() + "depois Y: " + map.getY());
 
+	}
+	
+	private void MoveAllCharacters(int x, int y) {
+		Character CurrentCharacter;
+		
+		for(int i = 0; i < characters.size(); i++) {
+			CurrentCharacter = characters.get(i);
+			CurrentCharacter.InDecLocale(x, y);
+		}
+	}
+	
+	private void setLocaleToAllCharacters(int x, int y) {
+		Character CurrentCharacter;
+		
+		for(int i = 0; i < characters.size(); i++) {
+			CurrentCharacter = characters.get(i);
+			CurrentCharacter.Locale(x, y);
+		}
 	}
 	
 }
