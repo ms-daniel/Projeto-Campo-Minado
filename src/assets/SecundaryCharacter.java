31d0601e5b9c0c22cd71726538fad065bd721e44
Carlos Daniel <ms_danniel@outlook.com>
2023-05-08 16:49:36 -0300

    criação de metodos e conexao com servidor

--- src/assets/SecundaryCharacter.java
+++ src/assets/SecundaryCharacter.java
@@ -1,15 +1,62 @@
 package assets;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 import javax.swing.JLabel;
 
 public class SecundaryCharacter extends Character{
-	public SecundaryCharacter(String name, String SkinName, JLabel SkinLabel, JLabel PlayerName) {
+	private int toX = 0;
+	private int toY = 0;
+	
+	private Socket connection;
+	
+	private BufferedReader inFromServer;
+	private InputStream toServer;
+	
+	public SecundaryCharacter(String name, String SkinName, JLabel SkinLabel, JLabel PlayerName, Socket socket) {
 		super(name, SkinName, SkinLabel, PlayerName);
+		this.connection = socket;
+		
+		try {
+			this.inFromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void run() {
 		// TODO Criar movimentação feita pelo servidor
+	}
+	
+	/**
+	 * Esse metodo deve ser chamado pelo servidor 
+	 * para mover os personagens dentro do mapa
+	 * @param x: coordenada x para mover
+	 * @param y: coordenada y para mover
+	 */
+	public void ToMove(int x, int y) {
+		//verifica se irá pra esquerda/direita ou cima/baixo
+		if(x != this.CoordenateX) {
+			if(x > this.CoordenateX)
+				toX = 45;
+			else
+				toX = -45;
+		}
+		else {
+			if(y > this.CoordenateX)
+				toY = 45;
+			else
+				toY = -45;
+		}
+		
+		this.CoordenateX = x;
+		this.CoordenateY = y;
 	}
 	
 	/**
