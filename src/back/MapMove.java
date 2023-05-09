1e1ee96969f6cfc4057b602644ac4df01b565c98
Carlos Daniel <ms_danniel@outlook.com>
2023-05-08 22:08:12 -0300

    -Arraylist, +Array

--- src/back/MapMove.java
+++ src/back/MapMove.java
@@ -11,26 +11,33 @@ public class MapMove implements Runnable {
     private int x;
     private int y;
     private int increment;
-    private ArrayList<SecundaryCharacter> characters = new ArrayList<>();;
+    
+    private SecundaryCharacter[] characters = new SecundaryCharacter[4];
+    private int QTPlayers = 0;
     
     /**
      * Class responsible for moving the map of other players
      * @param map: Image of the main map
      * @param mapT: Usually the second layer of the map containing the tops of trees, mountains, etc.
      */
-	public MapMove(JLabel map, JLabel mapT) {
-        this.map = map;
-        this.mapT = mapT;
-    }
+//	public MapMove(JLabel map, JLabel mapT) {
+//        this.map = map;
+//        this.mapT = mapT;
+//    }
 	
     /**
      * Class responsible for moving the map of other players
      * @param map: Image of the main map
      */
-	public MapMove(JLabel map) {
+//	public MapMove(JLabel map) {
+//        this.map = map;
+//        this.mapT = null;
+//    }
+	
+	public void setMaps(JLabel map, JLabel mapT) {
         this.map = map;
-        this.mapT = null;
-    }
+        this.mapT = mapT;
+	}
 	
 	/**
 	 * Tells where the map should be moved and whether it's a negative (up) or positive (down) move
@@ -53,7 +60,8 @@ public class MapMove implements Runnable {
 	 * @param Character: player
 	 */
 	public void AddCharacterToMap(SecundaryCharacter Character) {
-		this.characters.add(Character);
+		this.characters[QTPlayers] = Character;
+		QTPlayers++;
 	}
 	
 	/**
@@ -67,17 +75,15 @@ public class MapMove implements Runnable {
 		//This for is responsible for the movement of the map while it shows the animation of the main character walking
 		for(int i = 1; Math.abs(i) < 46; i+=increment) {
 			if(y == 0) {
+				MoveAllCharacters(increment, 0);
 				map.setLocation(map.getX() + increment, map.getY());
 				if(mapT != null)
 					mapT.setLocation(mapT.getX() + increment, mapT.getY());
-				
-				MoveAllCharacters(increment, 0);
 			}else {
+				MoveAllCharacters(0, increment);
 				map.setLocation(map.getX(), map.getY() + increment);
 				if(mapT != null)
 					mapT.setLocation(mapT.getX(), mapT.getY() + increment);	
-				
-				MoveAllCharacters(0, increment);
 			}
 
 			try {
@@ -89,17 +95,16 @@ public class MapMove implements Runnable {
 		}
 		//=======================================================================
 		
-		//position the maps in their final places
-		map.setLocation(xAux, yAux);
-		if(mapT != null)
-			mapT.setLocation(xAux, yAux);
-		
 		//positions all players within the map in their final places
 		if(y == 0)
 			setLocaleToAllCharacters(45*increment, 0);
 		else
 			setLocaleToAllCharacters(0, 45*increment);
-		
+				
+		//position the maps in their final places
+		map.setLocation(xAux, yAux);
+		if(mapT != null)
+			mapT.setLocation(xAux, yAux);
 	}
 	
 	/**
@@ -110,8 +115,8 @@ public class MapMove implements Runnable {
 	private synchronized void MoveAllCharacters(int x, int y) {
 		SecundaryCharacter CurrentCharacter;
 		
-		for(int i = 0; i < characters.size(); i++) {
-			CurrentCharacter = characters.get(i);
+		for(int i = 0; i < QTPlayers; i++) {
+			CurrentCharacter = characters[i];
 			CurrentCharacter.IncrementLocale(x, y);
 		}
 	}
@@ -124,8 +129,8 @@ public class MapMove implements Runnable {
 	private synchronized void setLocaleToAllCharacters(int x, int y) {
 		SecundaryCharacter CurrentCharacter;
 		
-		for(int i = 0; i < characters.size(); i++) {
-			CurrentCharacter = characters.get(i);
+		for(int i = 0; i < QTPlayers; i++) {
+			CurrentCharacter = characters[i];
 			CurrentCharacter.Locale(x, y);
 		}
 	}
